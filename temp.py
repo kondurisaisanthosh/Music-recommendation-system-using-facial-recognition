427b956cf47381be6eb6156f95fb0350c87db230
skonduri <skonduri@albany.edu>
2021-02-19 11:37:21 -0500

    temp

---
+++ temp.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import tensorflow.keras
+from tensorflow import keras
+
+print(np.version.version)
+print(pd. __version__)
+print(tensorflow.__version__)
+print(keras.__version__)
