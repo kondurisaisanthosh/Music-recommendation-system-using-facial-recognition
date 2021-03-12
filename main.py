1a60ded60e4e93ec23b3ccb9a46832355cf703a6
skonduri <skonduri@albany.edu>
2021-03-12 02:57:52 -0500

    camera

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten
 from keras.layers import Conv2D,MaxPooling2D,BatchNormalization
 from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam, SGD
+from keras.optimizers import   RMSprop
 from keras.regularizers import l2
 from keras.utils import np_utils
 
@@ -43,7 +44,7 @@ Y_test=np.array(Y_test,'float32')
 num_features=64
 num_labels=7
 batch_size=256
-epochs=100
+epochs=30
 width,height=48,48
 
 Y_train=np_utils.to_categorical(Y_train , num_classes=num_labels)
@@ -53,7 +54,6 @@ Y_test=np_utils.to_categorical(Y_test, num_classes=num_labels)
 
 X_train/=255
 X_test/=255
-
 
 
 X_train=X_train.reshape(X_train.shape[0],width,height,1)
@@ -72,13 +72,13 @@ model.add(MaxPooling2D(pool_size=(5,5), strides=(2, 2)))
 
 #2nd convolution layer
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+#model.add(Conv2D(64, (3, 3), activation='relu'))
 # model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(3,3), strides=(2, 2)))
 
 #3rd convolution layer
 model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
+#model.add(Conv2D(128, (3, 3), activation='relu'))
 # model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(3,3), strides=(2, 2)))
 
@@ -93,10 +93,9 @@ model.add(Dropout(0.2))
 model.add(Dense(num_labels, activation='softmax'))
 
 # model.summary()
-
 #Compliling the model
 model.compile(loss=categorical_crossentropy,
-              optimizer=Adam(),
+              optimizer=RMSprop(),
               metrics=['accuracy'])
 
 #Training the model
