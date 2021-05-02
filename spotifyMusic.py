effa438322ccee3bcf76bdbbdf134fb33d48df03
skonduri <skonduri@albany.edu>
2021-05-02 17:17:14 -0400

    new music player

--- spotifyMusic.py
+++ spotifyMusic.py
@@ -1,26 +1,17 @@
 # Referenced from https://sotiriskakanos.com/2017/08/05/using-spotifys-web-api-with-python/
+
 #Import required libraries
 import spotipy
 import webbrowser
 from spotipy.oauth2 import SpotifyClientCredentials
 
 #Authorize API using credentials
-client_credentials_manager = SpotifyClientCredentials(client_id='c05f99ee0a2d4c059c835f8f4cbe2c73', client_secret='596ed849c5a44891af5fdd1f74ba8365')
+client_credentials_manager = SpotifyClientCredentials(client_id='b86bcb98e728443e9a690bf58572f53e', client_secret='ed42ae66555c4b01b3ab988efc6c4219')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-playlists = sp.user_playlists('li006zvua0ttqvdzzns2gwtu5') #My User ID/Name
+playlists = sp.user_playlists('rmnvd22tpuikmwf5yzzsgnu2t') #My User ID/Name
 
-print(playlists)
-# playlists = sp.user_playlists('dale_xi')
-# playlists = sp.user_playlists('MartinStoichkov')
-
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None
+# print(playlists)
 
 def show_tracks(tracks):
     for i, item in enumerate(tracks['items']):
@@ -28,12 +19,13 @@ def show_tracks(tracks):
         print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
 
 for playlist in playlists['items']:
-        if playlist['owner']['id'] == 'li006zvua0ttqvdzzns2gwtu5':
+        if playlist['owner']['id'] == 'rmnvd22tpuikmwf5yzzsgnu2t' and playlist['name']=='disgust':
             print(playlist['name'])
             print(playlist['external_urls']['spotify'])
-            webbrowser.open(playlist['external_urls']['spotify'])
+            # webbrowser.open(playlist['external_urls']['spotify'])
             print('  total tracks', playlist['tracks']['total'])
-            results = sp.user_playlist('li006zvua0ttqvdzzns2gwtu5', playlist['id'], fields="tracks,next")
+
+            results = sp.user_playlist('rmnvd22tpuikmwf5yzzsgnu2t', playlist['id'], fields="tracks,next")
             tracks = results['tracks']
             show_tracks(tracks)
             while tracks['next']:
