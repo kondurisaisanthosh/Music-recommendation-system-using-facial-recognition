3074de05e6e620d71bed9ad7788fc049c35ed6b7
skonduri <skonduri@albany.edu>
2021-05-06 10:59:38 -0400

    new

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -8,20 +8,26 @@ let playlists = new Map();
 playlists.set("happy","https://open.spotify.com/embed/playlist/5MPIVMVxSdXXXiwBQM6a45");
 playlists.set("sad","https://open.spotify.com/embed/playlist/3NErR0TbvorTmM65GptZlP");
 playlists.set("surprise","https://open.spotify.com/embed/playlist/6hMG5j7zDiSZij7NDIfDdW");
-playlists.set("neutral","https://open.spotify.com/playlist/5kXFRi7s7it02gge12kGra");
-playlists.set("disgust","https://open.spotify.com/playlist/4bSG6gA0zPVgiqKGStSkzV");
+playlists.set("neutral","https://open.spotify.com/embed/playlist/5kXFRi7s7it02gge12kGra");
+playlists.set("disgust","https://open.spotify.com/embed/playlist/4bSG6gA0zPVgiqKGStSkzV");
 playlists.set("fear","https://open.spotify.com/embed/playlist/3ohYn6zM5RiqNPx4YVz3Re");
 playlists.set("angry","https://open.spotify.com/embed/playlist/4SDCW3gQcNlD37XsM5ca6D");
-playlists.set("NO VALUE","https://open.spotify.com/embed/playlist/4SDCW3gQcNlD37XsM5ca6D");
+playlists.set("NO VALUE","https://open.spotify.com/embed/playlist/5MPIVMVxSdXXXiwBQM6a45");
 let playlist;
 
 Webcam.attach("#camera");
 function take_snapshot(){
     Webcam.snap(function(data_uri){
+        document.getElementById('camera').innerHTML = '<img src="'+data_uri+'"/>';
         var base64data=data_uri.split(",")[1];
         var newbase64img=base64data.replace(/\//g,"SLASH");
-        getEmotion(newbase64img).then(data=>{playlist=playlists.get(data);console.log(playlist);document.getElementById("spotify").src=playlist});
-    });
+        getEmotion(newbase64img).then(data=>{
+                                playlist=playlists.get(data);
+                                console.log(playlist);
+                                document.getElementById("spotify").src=playlist;
+                                document.getElementById("Mood").innerHTML="Person is "+data;
+                            });
+        });
 }
 
 async function getEmotion(newbase64img){
